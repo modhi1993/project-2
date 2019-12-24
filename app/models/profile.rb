2bcaa961069dc78137ebc0199407161f3401c6e3
Modhi Aldughaiyem <modhi1993@gmail.com>
2019-12-24 22:46:00 +0300

    fifth version

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,4 @@
 class Profile < ApplicationRecord
-  belongs_to :city
-  belongs_to :user
+  has_one :city 
+  has_one :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :hauls, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one :profile, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
